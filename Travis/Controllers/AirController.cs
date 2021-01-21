f62f93073b2b8517dd5d73ba4b1e6d4c64830bf8
FakePlasticTrees <vivaladaman@gmail.com>
2021-01-21 15:56:44 +0700

    WIP

---
+++ Travis/Controllers/AirController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Travis.Controllers
+{
+    [ApiController]
+    public class AirController : ControllerBase
+    {
+        [HttpGet("")]
+        public void Now(GeoCode geoCode)
+        {
+            return null;
+        }
+    }
+}
