ab42823aac46b88d35e0454934a7517749db5b6a
FakePlasticTrees <vivaladaman@gmail.com>
2021-01-23 13:56:01 +0700

    wip

---
+++ Travis/Controllers/PasswordManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Travis.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PasswordManager : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult List()
+        {
+
+        }
+    }
+}
