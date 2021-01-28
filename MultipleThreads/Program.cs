394150a8cbe700daa62e5378f77d906b7c2f3df7
FakePlasticTrees <vivaladaman@gmail.com>
2021-01-28 16:28:38 +0700

    init electron working with web socket

---
+++ MultipleThreads/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace MultipleThreads
+{
+    class Program
+    {
+        const int Iterations = 1000000;
+
+        static int counter;
+
+
+        static void Main()
+        {
+            Thread t1 = new Thread(AddLots);
+            Thread t2 = new Thread(AddLots);
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine(counter);
+            Console.ReadLine();
+        }
+
+        static void AddLots()
+        {
+            for (int i = 0; i < Iterations; i++)
+            {
+                // Broken!
+                //counter++;
+
+                //Thread safe
+                Interlocked.Increment(ref counter);
+            }
+        }
+    }
+}
