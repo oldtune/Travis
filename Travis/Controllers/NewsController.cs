c81231d6c46dc3b52c1e385af79585f80ccf7b5c
FakePlasticTrees <vivaladaman@gmail.com>
2021-01-28 17:18:15 +0700

    add news controller

---
+++ Travis/Controllers/NewsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Travis.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NewsController : ControllerBase
+    {
+        public NewsController()
+        {
+
+        }
+
+        [HttpGet("")]
+        public IActionResult Fetch()
+        {
+            return Ok();
+        }
+
+        [HttpGet("force-get")]
+        public IActionResult ForceFetch()
+        {
+            return Ok();
+        }
+    }
+}
