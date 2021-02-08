5d7363eab29793c9ac38696b1b049b9579f6008e
FakePlasticTrees <vivaladaman@gmail.com>
2021-02-08 15:41:51 +0700

    wip

---
+++ Domain/ValueObjects/SitePasswords.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects
+{
+    public class SitePasswords
+    {
+        public string Password { set; get; }
+        public string Username { set; get; }
+        public string UsedByEmail { set; get; }
+        public string UsedByPhone { set; get; }
+        public string Site { set; get; }
+    }
+}
---
+++ Travis/Controllers/SitePasswordController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Travis.Controllers
+{
+    /// <summary>
+    /// Well, use randomized code issued by another provider (another api, but my as well)
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class SitePasswordController : ControllerBase
+    {
+        /// <summary>
+        /// Get with code to secure your system
+        /// </summary>
+        /// <param name="code"></param>
+        [HttpGet("")]
+        public IActionResult FetchAll(string code, string query)
+        {
+            //expression trees to the rescue to query all string field (propably all fields in this aggregate are string)
+            return Ok();
+        }
+
+        [HttpPost("")]
+        public IActionResult Add()
+        {
+            return Ok();
+        }
+
+        [HttpPut("")]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("")]
+        public IActionResult Remove()
+        {
+            return Ok();
+        }
+    }
+}
